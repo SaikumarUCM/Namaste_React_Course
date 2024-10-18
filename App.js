ca6f74d03cfc0020fe34309d4125b5931af4edd9
Pasupuleti Sai Kumar <125770588+SaikumarUCM@users.noreply.github.com>
2024-10-18 10:30:30 -0500

    React update

--- App.js
+++ App.js
@@ -2,22 +2,3 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const parent=React.createElement(
-    "div",
-    {id:'parent'},
-    [React.createElement(
-        "div",
-        {id:'child1'},
-        [React.createElement("h1",{},"Namaste React"),React.createElement("h2",{},"I am h2 tag")]
-    ),React.createElement(
-    "div",
-    {id:'child2'},
-    [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
-)]);
-
-console.log(parent);
-    
-
-const root= ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
